fedf6a76dde7f6091e85183d912a1c3906c76ac1
ramror <ramesh.ramror@gmail.com>
2018-06-04 14:28:34 +0530

    Worked on edit and delete

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,6 @@
 class PlaysController < ApplicationController
   before_action :find_play, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @plays = Play.all.order("created_at DESC")
   end
@@ -20,6 +20,22 @@ class PlaysController < ApplicationController
     else
       render 'new'
     end        
+  end
+
+  def edit
+  end
+
+  def update
+    if @play.update(play_params)
+      redirect_to play_path(@play)
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @play.destroy
+    redirect_to root_path
   end
 
   private
